addf663bdd56d1752e38074455f31e012754b85d
gs <katharos@bk.ru>
2024-05-13 19:11:20 +0700

    chore: language control constructions moved to enum `ControlStructureName`

---
+++ src/ControlStructureName.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Che\SimpleLisp;
+
+enum ControlStructureName: string
+{
+    case COND = 'cond';
+    case DEF = 'def';
+    case SET = 'set!';
+    case DO = 'do';
+    case QUOTE = 'quote';
+    case LAMBDA = 'lambda';
+    case MACRO = 'macro';
+    case EVAL = 'eval';
+    case TYPEOF = 'typeof';
+    case PRINT = 'print';
+}
